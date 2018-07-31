25fdf9d0af78425ee2c63e20e15d79b959e8fc7b
Unknown <ilee@scisolutions.com>
2018-07-30 22:49:27 -0700

    move process stage result to process namespace

--- Source/RunInfoBuilder/Pipeline/Models/ProcessStageResult.cs
+++ Source/RunInfoBuilder/Pipeline/Models/ProcessStageResult.cs
@@ -1,39 +1,4 @@
 ﻿namespace R5.RunInfoBuilder
 {
-	internal enum AfterProcessingStage
-	{
-		Continue,
-		StopProcessingRemainingStages,
-		KillBuild
-	}
 
-	public class ProcessStageResult
-	{
-		internal int SkipNextCount { get; private set; }
-		internal AfterProcessingStage AfterProcessing { get; private set; }
-
-		public ProcessStageResult()
-		{
-			SkipNextCount = 0;
-			AfterProcessing = AfterProcessingStage.Continue;
-		}
-
-		public ProcessStageResult SkipNext(int skip)
-		{
-			SkipNextCount = skip;
-			return this;
-		}
-
-		public ProcessStageResult StopProcessingCurrentArgument()
-		{
-			AfterProcessing = AfterProcessingStage.StopProcessingRemainingStages;
-			return this;
-		}
-
-		public ProcessStageResult KillBuildProcess()
-		{
-			AfterProcessing = AfterProcessingStage.KillBuild;
-			return this;
-		}
-	}
 }
--- Source/RunInfoBuilder/Pipeline/PipelineProcessor.cs
+++ Source/RunInfoBuilder/Pipeline/PipelineProcessor.cs
@@ -1,165 +1,165 @@
-﻿using R5.RunInfoBuilder.ArgumentParser;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using R5.RunInfoBuilder.Store;
-using R5.RunInfoBuilder.Configuration;
+﻿//using R5.RunInfoBuilder.ArgumentParser;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using R5.RunInfoBuilder.Store;
+//using R5.RunInfoBuilder.Configuration;
 
-namespace R5.RunInfoBuilder.Pipeline
-{
-	internal interface IPipelineProcessor<TRunInfo>
-		where TRunInfo : class
-	{
-		void ProcessArgs(List<ProgramArgument> programArgumentInfos);
-	}
+//namespace R5.RunInfoBuilder.Pipeline
+//{
+//	internal interface IPipelineProcessor<TRunInfo>
+//		where TRunInfo : class
+//	{
+//		void ProcessArgs(List<ProgramArgument> programArgumentInfos);
+//	}
 
-	internal class PipelineProcessor<TRunInfo> : IPipelineProcessor<TRunInfo>
-		where TRunInfo : class
-	{
-		enum AfterProcessingArgument
-		{
-			Continue,
-			KillBuild
-		}
+//	internal class PipelineProcessor<TRunInfo> : IPipelineProcessor<TRunInfo>
+//		where TRunInfo : class
+//	{
+//		enum AfterProcessingArgument
+//		{
+//			Continue,
+//			KillBuild
+//		}
 
-		private RunInfo<TRunInfo> _runInfo { get; }
-		private ProcessConfig _processConfig { get; }
-		//private List<ProcessPipelineStageBase<TRunInfo>> _pipeline { get; set; }
-		private Action<PreProcessContext<TRunInfo>> _preProcessCallback { get; set; }
-		private Action<PostProcessContext<TRunInfo>> _postProcessCallback { get; set; }
+//		private RunInfo<TRunInfo> _runInfo { get; }
+//		private ProcessConfig _processConfig { get; }
+//		//private List<ProcessPipelineStageBase<TRunInfo>> _pipeline { get; set; }
+//		private Action<PreProcessContext<TRunInfo>> _preProcessCallback { get; set; }
+//		private Action<PostProcessContext<TRunInfo>> _postProcessCallback { get; set; }
 
-		public PipelineProcessor(
-			IArgumentMetadata<TRunInfo> argumentMaps,
-			RunInfo<TRunInfo> runInfo,
-			IParser parser,
-			IArgumentTokenizer tokenizer,
-			ProcessConfig processConfig,
-			HooksConfig<TRunInfo> hooksConfig)
-		{
-			_runInfo = runInfo;
-			_processConfig = processConfig;
+//		public PipelineProcessor(
+//			IArgumentMetadata<TRunInfo> argumentMaps,
+//			RunInfo<TRunInfo> runInfo,
+//			IParser parser,
+//			IArgumentTokenizer tokenizer,
+//			ProcessConfig processConfig,
+//			HooksConfig<TRunInfo> hooksConfig)
+//		{
+//			_runInfo = runInfo;
+//			_processConfig = processConfig;
 
-			//_pipeline = new List<ProcessPipelineStageBase<TRunInfo>>
-			//{
-			//	//new CommandStage<TRunInfo>(argumentMaps, runInfo),
-			//	new OptionStage<TRunInfo>(argumentMaps, runInfo, tokenizer),
-			//	//new ArgumentStage<TRunInfo>(argumentMaps, runInfo, parser, tokenizer)
-			//};
+//			//_pipeline = new List<ProcessPipelineStageBase<TRunInfo>>
+//			//{
+//			//	//new CommandStage<TRunInfo>(argumentMaps, runInfo),
+//			//	new OptionStage<TRunInfo>(argumentMaps, runInfo, tokenizer),
+//			//	//new ArgumentStage<TRunInfo>(argumentMaps, runInfo, parser, tokenizer)
+//			//};
 
-			Configure(hooksConfig);
-		}
+//			Configure(hooksConfig);
+//		}
 
-		private void Configure(HooksConfig<TRunInfo> config)
-		{
-			_preProcessCallback = config.PreBuildCallback;
-			_postProcessCallback = config.PostBuildCallback;
+//		private void Configure(HooksConfig<TRunInfo> config)
+//		{
+//			_preProcessCallback = config.PreBuildCallback;
+//			_postProcessCallback = config.PostBuildCallback;
 
-			//if (config.PreArgumentCallback != null)
-			//{
-			//	_pipeline.Insert(0, new PreProcessArgumentStage<TRunInfo>(config.PreArgumentCallback));
-			//}
+//			//if (config.PreArgumentCallback != null)
+//			//{
+//			//	_pipeline.Insert(0, new PreProcessArgumentStage<TRunInfo>(config.PreArgumentCallback));
+//			//}
 
-			//if (config.PostArgumentCallback != null)
-			//{
-			//	_pipeline.Add(new PostProcessArgumentStage<TRunInfo>(config.PostArgumentCallback));
-			//}
-		}
+//			//if (config.PostArgumentCallback != null)
+//			//{
+//			//	_pipeline.Add(new PostProcessArgumentStage<TRunInfo>(config.PostArgumentCallback));
+//			//}
+//		}
 
-		public void ProcessArgs(List<ProgramArgument> programArguments)
-		{
-			if (_preProcessCallback != null)
-			{
-				var preProcessContext = new PreProcessContext<TRunInfo>(GetProgramArgumentsFrom(programArguments), _runInfo.Value);
-				_preProcessCallback(preProcessContext);
-			}
+//		public void ProcessArgs(List<ProgramArgument> programArguments)
+//		{
+//			if (_preProcessCallback != null)
+//			{
+//				var preProcessContext = new PreProcessContext<TRunInfo>(GetProgramArgumentsFrom(programArguments), _runInfo.Value);
+//				_preProcessCallback(preProcessContext);
+//			}
 
-			for (int i = 0; i < programArguments.Count; i++)
-			{
-				ProgramArgument info = programArguments[i];
+//			for (int i = 0; i < programArguments.Count; i++)
+//			{
+//				ProgramArgument info = programArguments[i];
 
-				if (info.Type == ProgramArgumentType.Unresolved)
-				{
-					ThrowIfUnresolvedNotAllowed(info, _processConfig);
-					continue;
-				}
+//				if (info.Type == ProgramArgumentType.Unresolved)
+//				{
+//					ThrowIfUnresolvedNotAllowed(info, _processConfig);
+//					continue;
+//				}
 
-				//ProcessContext<TRunInfo> stageContext = GetStageContext(info, GetProgramArgumentsFrom(programArguments), _runInfo.Value);
+//				//ProcessContext<TRunInfo> stageContext = GetStageContext(info, GetProgramArgumentsFrom(programArguments), _runInfo.Value);
 
-				//(int skipNext, AfterProcessingArgument afterArgument) = this.ProcessArgumentInPipeline(
-				//	info.ArgumentToken, stageContext);
+//				//(int skipNext, AfterProcessingArgument afterArgument) = this.ProcessArgumentInPipeline(
+//				//	info.ArgumentToken, stageContext);
 
-				//i += skipNext;
+//				//i += skipNext;
 
-				//if (afterArgument == AfterProcessingArgument.KillBuild)
-				//{
-				//	break;
-				//}
-			}
+//				//if (afterArgument == AfterProcessingArgument.KillBuild)
+//				//{
+//				//	break;
+//				//}
+//			}
 
-			if (_postProcessCallback != null)
-			{
-				var postProcessContext = new PostProcessContext<TRunInfo>(GetProgramArgumentsFrom(programArguments), _runInfo.Value);
-				_postProcessCallback(postProcessContext);
-			}
-		}
+//			if (_postProcessCallback != null)
+//			{
+//				var postProcessContext = new PostProcessContext<TRunInfo>(GetProgramArgumentsFrom(programArguments), _runInfo.Value);
+//				_postProcessCallback(postProcessContext);
+//			}
+//		}
 
-		// the program arguments made available to users are essentially "immutable"
-		// by giving a fresh copy at each accessible place
-		private static string[] GetProgramArgumentsFrom(List<ProgramArgument> infos) => infos
-				.Select(i => i.ArgumentToken)
-				.ToArray();
+//		// the program arguments made available to users are essentially "immutable"
+//		// by giving a fresh copy at each accessible place
+//		private static string[] GetProgramArgumentsFrom(List<ProgramArgument> infos) => infos
+//				.Select(i => i.ArgumentToken)
+//				.ToArray();
 
-		private static void ThrowIfUnresolvedNotAllowed(ProgramArgument info, ProcessConfig config)
-		{
-			switch (config.HandleUnresolved)
-			{
-				case HandleUnresolvedArgument.NotAllowed:
-					// should never reach here due to validation before this
-					throw new InvalidOperationException("Unresolved program arguments are invalid for this configuration.");
-				case HandleUnresolvedArgument.AllowedButThrowOnProcess:
-					throw new RunInfoBuilderException($"Failed to process program argument '{info.ArgumentToken}' because it's an unknown type.");
-				case HandleUnresolvedArgument.AllowedButSkipOnProcess:
-					break;
-				default:
-					throw new ArgumentOutOfRangeException($"'{config.HandleUnresolved}' is invalid.");
-			}
-		}
+//		private static void ThrowIfUnresolvedNotAllowed(ProgramArgument info, ProcessConfig config)
+//		{
+//			switch (config.HandleUnresolved)
+//			{
+//				case HandleUnresolvedArgument.NotAllowed:
+//					// should never reach here due to validation before this
+//					throw new InvalidOperationException("Unresolved program arguments are invalid for this configuration.");
+//				case HandleUnresolvedArgument.AllowedButThrowOnProcess:
+//					throw new RunInfoBuilderException($"Failed to process program argument '{info.ArgumentToken}' because it's an unknown type.");
+//				case HandleUnresolvedArgument.AllowedButSkipOnProcess:
+//					break;
+//				default:
+//					throw new ArgumentOutOfRangeException($"'{config.HandleUnresolved}' is invalid.");
+//			}
+//		}
 
-		//private static ProcessContext<TRunInfo> GetStageContext(ProgramArgument info,
-		//	string[] programArguments, TRunInfo runInfo) => new ProcessContext<TRunInfo>(
-		//		info.ArgumentToken,
-		//		info.Type,
-		//		info.Position,
-		//		programArguments,
-		//		runInfo);
+//		//private static ProcessContext<TRunInfo> GetStageContext(ProgramArgument info,
+//		//	string[] programArguments, TRunInfo runInfo) => new ProcessContext<TRunInfo>(
+//		//		info.ArgumentToken,
+//		//		info.Type,
+//		//		info.Position,
+//		//		programArguments,
+//		//		runInfo);
 
-		//private (int SkipNext, AfterProcessingArgument AfterArgument) ProcessArgumentInPipeline(string argumentToken,
-		//	ProcessContext<TRunInfo> processArgumentContext)
-		//{
-		//	int totalSkipNext = 0;
-		//	foreach (ProcessPipelineStageBase<TRunInfo> stage in _pipeline)
-		//	{
-		//		bool shouldProcess = stage.CanProcessArgument(processArgumentContext.ArgumentType);
-		//		if (!shouldProcess)
-		//		{
-		//			continue;
-		//		}
+//		//private (int SkipNext, AfterProcessingArgument AfterArgument) ProcessArgumentInPipeline(string argumentToken,
+//		//	ProcessContext<TRunInfo> processArgumentContext)
+//		//{
+//		//	int totalSkipNext = 0;
+//		//	foreach (ProcessPipelineStageBase<TRunInfo> stage in _pipeline)
+//		//	{
+//		//		bool shouldProcess = stage.CanProcessArgument(processArgumentContext.ArgumentType);
+//		//		if (!shouldProcess)
+//		//		{
+//		//			continue;
+//		//		}
 
-		//		(int skipNext, AfterProcessingStage afterStage) = stage.Process(processArgumentContext);
+//		//		(int skipNext, AfterProcessingStage afterStage) = stage.Process(processArgumentContext);
 
-		//		totalSkipNext += skipNext;
+//		//		totalSkipNext += skipNext;
 
-		//		if (afterStage == AfterProcessingStage.StopProcessingRemainingStages)
-		//		{
-		//			break;
-		//		}
-		//		if (afterStage == AfterProcessingStage.KillBuild)
-		//		{
-		//			return (skipNext, AfterProcessingArgument.KillBuild);
-		//		}
-		//	}
+//		//		if (afterStage == AfterProcessingStage.StopProcessingRemainingStages)
+//		//		{
+//		//			break;
+//		//		}
+//		//		if (afterStage == AfterProcessingStage.KillBuild)
+//		//		{
+//		//			return (skipNext, AfterProcessingArgument.KillBuild);
+//		//		}
+//		//	}
 
-		//	return (totalSkipNext, AfterProcessingArgument.Continue);
-		//}
-	}
-}
+//		//	return (totalSkipNext, AfterProcessingArgument.Continue);
+//		//}
+//	}
+//}
